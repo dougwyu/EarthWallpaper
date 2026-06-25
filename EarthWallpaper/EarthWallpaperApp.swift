844cef9e59fee8583eb7868da96239791633873c
Douglas Yu <dougwyu@mac.com>
2026-06-25 09:50:56 +0800

    feat: add @main app entry point with MenuBarExtra and Settings scenes

--- EarthWallpaper/EarthWallpaperApp.swift
+++ EarthWallpaper/EarthWallpaperApp.swift
@@ -2,9 +2,20 @@ import SwiftUI
 
 @main
 struct EarthWallpaperApp: App {
+    @StateObject private var appState = AppState()
+
     var body: some Scene {
+        MenuBarExtra("Earth", systemImage: "globe.americas.fill") {
+            MenuView()
+                .environmentObject(appState.cityStore)
+                .environmentObject(appState.updater)
+        }
+        .menuBarExtraStyle(.window)
+
         Settings {
-            EmptyView()
+            SettingsView()
+                .environmentObject(appState.cityStore)
+                .environmentObject(appState.updater)
         }
     }
 }
